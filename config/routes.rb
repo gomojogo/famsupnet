ce99b0ae7c919b61bc21c220d3a69cf9704bdf88
Leslie M. Gomez <lesliemgomezDBC@gmail.com>
2015-12-04 12:55:50 -0800

    most recent updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,7 @@ Rails.application.routes.draw do
   
 
 
-  get 'users/new'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
+  resources :users
 
   resources :categories do
     resources :trainings
